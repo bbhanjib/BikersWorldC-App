42339ecc172726e381516faf18820cfa8431df4a
bbhanjib <bineet.b@hotmail.co.uk>
2016-03-15 10:13:34 +0000

    sales form
    no datbase added yet

---
+++ unit 42/unit 42/frmHub.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace unit_42
+{
+    public partial class frmHub : Form
+    {
+        public frmHub()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btntechnicians_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnsales_Click(object sender, EventArgs e)
+        {
+            frmSales form2 = new frmSales();
+ 
+            form2.Show();
+            this.Hide();
+
+
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmHub_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
